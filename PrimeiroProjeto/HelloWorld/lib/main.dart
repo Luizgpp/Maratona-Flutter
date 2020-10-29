9920ac0ec9ee646efbd685d7d49c2781766dde5b
Luizgpp <luizgpp@gmail.com>
2020-10-28 23:09:50 -0300

    Criando class Widget e implemetando construtor

--- PrimeiroProjeto/HelloWorld/lib/main.dart
+++ PrimeiroProjeto/HelloWorld/lib/main.dart
@@ -2,13 +2,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-    child: Center(
-      child: Text(
-        'Hello World',
-        textDirection: TextDirection.rtl,
-        style: TextStyle(color: Colors.white, fontSize: 30.0),
+  runApp(AppWidget(title: 'Utilizando Construtor'));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.rtl,
+          style: TextStyle(color: Colors.white, fontSize: 30.0),
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
