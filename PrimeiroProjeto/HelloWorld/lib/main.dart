771e4bba42df8dc71088d4b058e5cb83104817e4
Luizgpp <luizgpp@gmail.com>
2020-10-28 23:47:34 -0300

    Contador utilizando SetState

--- PrimeiroProjeto/HelloWorld/lib/main.dart
+++ PrimeiroProjeto/HelloWorld/lib/main.dart
@@ -14,12 +14,35 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.red),
-      home: Container(
-        child: Center(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: GestureDetector(
           child: Text(
-            'Material Theme',
+            'Contador: $counter',
             style: TextStyle(fontSize: 35),
           ),
+          onTap: () {
+            setState(() {
+              counter++;
+            });
+          },
         ),
       ),
     );
